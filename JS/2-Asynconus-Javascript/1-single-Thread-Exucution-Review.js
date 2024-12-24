a92cf6b37051e707b542ebfad76c7604143f7e47
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-12-24 22:32:49 +0530

    Asynkronus

---
+++ JS/2-Asynconus-Javascript/1-single-Thread-Exucution-Review.js
@@ -0,0 +1,14 @@
+// Single Thread Exexution means ur code will do one task at a time;
+
+const num = 3;
+
+function multiplayBy2(inputNumber) {
+  const result = inputNumber * 2;
+  return result;
+}
+
+const output = multiplayBy2(num);
+const newOutput = multiplayBy2(10);
+
+console.log(output);
+console.log(newOutput);
