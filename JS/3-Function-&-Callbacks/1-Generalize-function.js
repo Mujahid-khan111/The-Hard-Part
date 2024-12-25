5d89b3d350822e69d2b3cfa7e0a970d0aea886da
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-12-26 01:28:06 +0530

    Callbacks

---
+++ JS/3-Function-&-Callbacks/1-Generalize-function.js
@@ -0,0 +1,9 @@
+// we can generalize the function to make it reusable
+
+function squaraNum(num) {
+  return num * num;
+}
+
+console.log(squaraNum(10));
+console.log(squaraNum(9));
+console.log(squaraNum(8));
