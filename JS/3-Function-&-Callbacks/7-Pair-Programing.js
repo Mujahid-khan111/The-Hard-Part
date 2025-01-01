24520a7f9bcc9fa84aab1ff20ae1283a5f4563ec
mujahid-khan111 <khanmujahid7003@gmail.com>
2025-01-01 21:00:37 +0530

    Pair-Programin

---
+++ JS/3-Function-&-Callbacks/7-Pair-Programing.js
@@ -0,0 +1,13 @@
+// Pair Programming ek aisa technique hota hai jisme do log ek hi computer pe
+// milke kaam karte hain. Ek banda code likhta hai aur doosra banda us code ko
+// review karta hai aur suggestions deta hai. Yeh dono kaam ek saath milke karte
+// hain taaki code zyada better ho.
+
+// Driver:(Code likhne wala):
+// Jo banda code likhta hai.
+// Keyboard aur mouse use karta hai.
+
+// Navigator:(Code ko review karne wala):
+// Jo banda code ko review karta hai.
+// Ideas deta hai aur code ko improve karne ke liye guide karta hai.
+
