977bebd241edc8ed64d65beb6a8aa1b1b79a9c1f
mujahid-khan111 <khanmujahid7003@gmail.com>
2025-01-08 01:31:47 +0530

    Generalize-Fun.

---
+++ JS/Generalized-Function/1-Generalize-Function.js
@@ -0,0 +1,27 @@
+// JavaScript mein Generalized Function ek aisi function hoti hai jo ek hi logic ke zariye
+// multiple inputs ya scenarios handle kar sakti hai.Iska main purpose code
+// reusability aur flexibility ko badhava dena hota hai. Generalized Functions ko commonly
+// parameterized functions ke roop mein likha jata hai jo different arguments ke saath kaam karte hain.
+
+function tensqured() {
+  return 10 * 10;
+}
+
+console.log(tensqured()); //100
+
+//=> 9 Squred
+
+function nineSqure() {
+  return 9 * 9;
+}
+
+console.log(nineSqure()); //81
+
+function squreNum(num){
+    return num * num;
+}
+
+console.log(10);
+console.log(8);
+console.log(6);
+console.log(2);
