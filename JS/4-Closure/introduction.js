3042fd40324c5c4a8af7bce6a77b144ee32fe908
mujahid-khan111 <khanmujahid7003@gmail.com>
2025-01-07 22:58:17 +0530

    Intro

---
+++ JS/4-Closure/introduction.js
@@ -0,0 +1,8 @@
+// Simple JavaScript example
+console.log("Hello, World!"); // Console me message print karega
+
+// Button click event
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button Clicked!");
+});
+
